f64eb3ac1d4b2340fdd3d56219cc631a393905f8
TsoiVadim <tsoivadim07@gmail.com>
2024-10-15 12:28:34 +0900

    test

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -6,4 +6,4 @@ def admin_user(message: Message, bot: TeleBot):
     """
     You can create a function and use parameter pass_bot.
     """
-    bot.send_message(message.chat.id, "Hello, admin!")
+    bot.send_message(message.chat.id, "Hello, admin! You are admin!")
